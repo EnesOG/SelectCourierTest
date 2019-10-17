b1768e736e6ce8e63097932646e6c7a7ddd065b5
Enes Sagir <sagir_enes@hotmail.com>
2019-10-17 10:52:20 +0200

    removed

--- printTmp.bat
+++
@@ -1,6 +0,0 @@
-@echo off 
-cd A:\Projects\selectcourierprinterapp
-PDFtoPrinter.exe "A:\Projects\selectcourierprinterapp./pdf/dummy2-1571300423716.pdf" "NPI81541A (HP Color LaserJet MFP M281fdw)"
-PDFtoPrinter.exe "A:\Projects\selectcourierprinterapp./pdf/dummy1-1571300423717.pdf" "NPI81541A (HP Color LaserJet MFP M281fdw)"
-exit /b 0 
-pause>nul 
